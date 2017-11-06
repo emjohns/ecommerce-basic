e939a59a61f7e44ce82c3ac3213bc06f7b19c624
Emma Johnson <em.johns00@gmail.com>
2017-11-06 09:08:57 -0500

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  post 'add_to_cart' => 'cart#add_to_cart'
+
+  post 'order_complete' => 'cart#order_complete'
+
+  get 'view_order' => 'cart#view_order'
+
+  get 'checkout' => 'cart#checkout'
+
+  devise_for :users
+
+  get 'storefront/all_items'
+
+  get 'categorical' => 'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
+
+  root 'storefront#all_items'
+
+  resources :products
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
